e34b30f2cbb1c7caa02c9514b76d45dc95932ff2
Harold Rojas <harolrop14@gmail.com>
2022-07-22 14:36:24 -0500

    initial commit with starter spring boot project

---
+++ src/main/java/com/devdoteros/eventblog/EventBlogApiApplication.java
@@ -0,0 +1,13 @@
+package com.devdoteros.eventblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventBlogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventBlogApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devdoteros/eventblog/EventBlogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devdoteros.eventblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventBlogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
