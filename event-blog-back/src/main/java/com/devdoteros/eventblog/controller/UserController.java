df3617a9887437264cb34d0d2deabd60252c2da1
Harold Rojas <harolrop14@gmail.com>
2022-07-23 12:37:43 -0500

    validation for edit user

--- event-blog-back/src/main/java/com/devdoteros/eventblog/controller/UserController.java
+++ event-blog-back/src/main/java/com/devdoteros/eventblog/controller/UserController.java
@@ -49,22 +49,22 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.CREATED).body(userService.save(user));
     }
 
-    private ResponseEntity<Map<String, String>> validate(BindingResult result) {
-        Map<String, String> errors = new HashMap<>();
-        result.getFieldErrors().forEach(fieldError -> {
-            String errorField = fieldError.getField();
-            String defaultMessage = fieldError.getDefaultMessage();
-            errors.put(errorField, "El campo " + errorField + " " + defaultMessage);
-                }
-        );
-        return ResponseEntity.badRequest().body(errors);
-    }
-
     @PutMapping("/{userId}")
     public ResponseEntity<?> editUser(@Valid @RequestBody User user, @PathVariable Long userId, BindingResult result) {
+        // Validate filled fields
+        if (result.hasErrors()) {
+            return validate(result);
+        }
         Optional<User> optionalUser = userService.findById(userId);
         if (optionalUser.isPresent()) {
             User userDb = optionalUser.get();
+            // When the email you want to replace already exists.
+            if (!user.getEmail().equalsIgnoreCase(userDb.getEmail()) &&
+                    userService.findByEmail(user.getEmail()).isPresent() &&
+                    !user.getEmail().isEmpty()) {
+                return ResponseEntity.badRequest()
+                        .body(Collections.singletonMap("message", "Existe un usuario con ese correo"));
+            }
             userDb.setName(user.getName());
             userDb.setEmail(user.getEmail());
             userDb.setPassword(user.getPassword());
@@ -83,5 +83,16 @@ public class UserController {
             return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
         }
         return ResponseEntity.notFound().build();
+    }
+
+    private ResponseEntity<Map<String, String>> validate(BindingResult result) {
+        Map<String, String> errors = new HashMap<>();
+        result.getFieldErrors().forEach(fieldError -> {
+                    String errorField = fieldError.getField();
+                    String defaultMessage = fieldError.getDefaultMessage();
+                    errors.put(errorField, "El campo " + errorField + " " + defaultMessage);
+                }
+        );
+        return ResponseEntity.badRequest().body(errors);
     }
 }
