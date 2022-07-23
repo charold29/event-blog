76b89b9f77541ac6ce0b4d0a77e94952590d22ef
Harold Rojas <harolrop14@gmail.com>
2022-07-23 12:31:11 -0500

    validation until saveUser

--- event-blog-back/src/main/java/com/devdoteros/eventblog/controller/UserController.java
+++ event-blog-back/src/main/java/com/devdoteros/eventblog/controller/UserController.java
@@ -4,9 +4,11 @@ import com.devdoteros.eventblog.model.entity.User;
 import com.devdoteros.eventblog.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import javax.validation.Valid;
 import java.util.*;
 
 @RestController
@@ -18,14 +20,14 @@ public class UserController {
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public List<User> getAll(){
+    public List<User> getAll() {
         return userService.findAll();
     }
 
     @GetMapping("/{userId}")
-    public ResponseEntity<User> getUserById(@PathVariable Long userId){
+    public ResponseEntity<User> getUserById(@PathVariable Long userId) {
         Optional<User> optionalUser = userService.findById(userId);
-        if (optionalUser.isPresent()){
+        if (optionalUser.isPresent()) {
             User user = optionalUser.get();
             return ResponseEntity.ok(user);
         }
@@ -34,14 +36,34 @@ public class UserController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public ResponseEntity<User> saveUser(@RequestBody User user){
+    public ResponseEntity<?> saveUser(@Valid @RequestBody User user, BindingResult result) {
+        // Validate filled fields
+        if (result.hasErrors()) {
+            return validate(result);
+        }
+        // Validate repeated email
+        if (userService.findByEmail(user.getEmail()).isPresent()){
+            return ResponseEntity.badRequest()
+                    .body(Collections.singletonMap("message", "Existe un usuario con ese correo"));
+        }
         return ResponseEntity.status(HttpStatus.CREATED).body(userService.save(user));
     }
 
+    private ResponseEntity<Map<String, String>> validate(BindingResult result) {
+        Map<String, String> errors = new HashMap<>();
+        result.getFieldErrors().forEach(fieldError -> {
+            String errorField = fieldError.getField();
+            String defaultMessage = fieldError.getDefaultMessage();
+            errors.put(errorField, "El campo " + errorField + " " + defaultMessage);
+                }
+        );
+        return ResponseEntity.badRequest().body(errors);
+    }
+
     @PutMapping("/{userId}")
-    public ResponseEntity<?> editUser(@RequestBody User user, @PathVariable Long userId){
+    public ResponseEntity<?> editUser(@Valid @RequestBody User user, @PathVariable Long userId, BindingResult result) {
         Optional<User> optionalUser = userService.findById(userId);
-        if (optionalUser.isPresent()){
+        if (optionalUser.isPresent()) {
             User userDb = optionalUser.get();
             userDb.setName(user.getName());
             userDb.setEmail(user.getEmail());
@@ -54,9 +76,9 @@ public class UserController {
     }
 
     @DeleteMapping("/{userId}")
-    public ResponseEntity<?> deleteUser(@PathVariable Long userId){
+    public ResponseEntity<?> deleteUser(@PathVariable Long userId) {
         Optional<User> optionalUser = userService.findById(userId);
-        if (optionalUser.isPresent()){
+        if (optionalUser.isPresent()) {
             userService.deleteById(userId);
             return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
         }
--- event-blog-back/src/main/java/com/devdoteros/eventblog/model/entity/User.java
+++ event-blog-back/src/main/java/com/devdoteros/eventblog/model/entity/User.java
@@ -4,6 +4,9 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,14 +19,22 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank
     private String name;
 
+    @NotEmpty
+    @Email
+    @Column(unique = true)
     private String email;
 
+    @NotBlank
     private String password;
 
+    @NotEmpty
+    @Column(unique = true)
     private String username;
 
+    @NotBlank
     private int age;
 
     @OneToMany
