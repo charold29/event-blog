241d384be97aa9d801b54e1981176a621090b82f
Harold Rojas <harolrop14@gmail.com>
2022-07-25 11:51:17 -0500

    spring security-configuring httpsecurity

---
+++ event-blog-back/src/main/java/com/devdoteros/eventblog/config/SecurityConfiguration.java
@@ -0,0 +1,22 @@
+package com.devdoteros.eventblog.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                        .antMatchers("/admin").hasRole("ADMIN")
+                        .antMatchers("/user").hasAnyRole("ADMIN","USER")
+                        .antMatchers("/").permitAll()
+                        .and().formLogin();
+        return http.build();
+    }
+    
+}
