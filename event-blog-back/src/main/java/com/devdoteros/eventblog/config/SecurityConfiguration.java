4da0f0861073a99dbbdbb9ac34a1f9a1860e6209
charoldev-wsl2 <harolrop14@gmail.comD>
2022-07-26 14:05:47 -0500

    web security

--- event-blog-back/src/main/java/com/devdoteros/eventblog/config/SecurityConfiguration.java
+++ event-blog-back/src/main/java/com/devdoteros/eventblog/config/SecurityConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -49,5 +50,8 @@ public class SecurityConfiguration {
     }
 
     // configure Web security...
-
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().antMatchers("/ignore1", "/ignore2");
+    }
 }
